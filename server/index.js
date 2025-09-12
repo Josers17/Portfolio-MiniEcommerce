fa911177e2fc247ba810d6cc35a8fdde7dcd416b
Jose Morales <josea.91@hotmail.com>
2025-09-12 15:20:21 -0400

    Add products API endpoint

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("API is working");
 });
 
+app.get("/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Shirt", price: 20 },
+    { id: 2, name: "Pants", price: 30 },
+    { id: 3, name: "Shoes", price: 50 }
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
