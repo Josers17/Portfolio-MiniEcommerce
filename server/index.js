9887936cdb3c499b5aa34688c4748913091c9aa8
Jose Morales <josea.91@hotmail.com>
2025-09-12 15:29:20 -0400

    Add products API endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = 5000;
+
+app.use(cors()); // habilita CORS
 
 app.get("/", (req, res) => {
   res.send("API is working");
